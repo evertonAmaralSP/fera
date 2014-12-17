c44040e4eca168c39dd6df4d9bf10e8795080628
Everton Amaral <everton.amaral@abril.com.br>
2014-12-17 20:30:58 -0200

    tela de reprocesso

--- src/main/java/br/com/abril/mamute/controller/SourceController.java
+++ src/main/java/br/com/abril/mamute/controller/SourceController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +19,10 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.abril.mamute.dao.ProductDAO;
 import br.com.abril.mamute.dao.SourceDAO;
 import br.com.abril.mamute.dao.TemplateDAO;
+import br.com.abril.mamute.exception.editorial.base.ComunicacaoComEditorialException;
+import br.com.abril.mamute.model.ResultadoBuscaMateria;
 import br.com.abril.mamute.model.Source;
+import br.com.abril.mamute.service.edtorial.Editorial;
 
 /**
  * Handles requests for the product home page.
@@ -27,9 +31,10 @@ import br.com.abril.mamute.model.Source;
 @RequestMapping("/sources")
 public class SourceController {
 	
-
-	final String SOURCE_LIST = "sources/SourceList";
-	final String SOURCE_FORM = "sources/SourceForm";
+	private static final String REDIRECT_SOURCES = "redirect:/sources/";
+	private static final String SOURCE_LIST = "sources/SourceList";
+	private static final String SOURCE_FORM = "sources/SourceForm";
+	private static final String SOURCE_RE = "sources/SourceRe";
 
 	@Autowired
 	private SourceDAO sourceDao;
@@ -37,6 +42,8 @@ public class SourceController {
 	private ProductDAO productDao;
 	@Autowired
 	private TemplateDAO templateDao;
+	@Autowired
+	private Editorial editorial;
 
 	@RequestMapping("/")
 	public ModelAndView handleRequest() throws Exception {
@@ -54,9 +61,9 @@ public class SourceController {
 		return SOURCE_FORM;
 	}
 
-	@RequestMapping(value = "/edit", method = RequestMethod.GET)
-	public String editSource(ModelMap model, HttpServletRequest request) {
-		int sourceId = Integer.parseInt(request.getParameter("id"));
+	@RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+	public String editSource(ModelMap model,@PathVariable String id) {
+		int sourceId = Integer.parseInt(id);
 		Source source = sourceDao.get(sourceId);
 		model.addAttribute("source", source);
 		model.addAttribute("listProduct", productDao.list());
@@ -65,30 +72,50 @@ public class SourceController {
 	}
 
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
-	public ModelAndView deleteSource(HttpServletRequest request) {
+	public String deleteSource(HttpServletRequest request) {
 		int sourceId = Integer.parseInt(request.getParameter("id"));
 		sourceDao.delete(sourceId);
-		return new ModelAndView("redirect:/sources/");
+		return REDIRECT_SOURCES;
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveSource(Model model, @Valid @ModelAttribute Source source, Errors errors) {
-		Boolean validateMarca = source.getProduct().getId() != null;
-		if (!validateMarca) {
-			errors.rejectValue("product", "validate.product.fail.mandatory_field");
-		}
-		Boolean validateTemplate = source.getTemplate().getId() != null;
-		if (!validateTemplate) {
-			errors.rejectValue("template", "validate.template.fail.mandatory_field");
-		}
-
+		validateProductId(source, errors);
+		validateTemplateId(source, errors);
 		if (errors.hasErrors()) {
+			model.addAttribute("listProduct", productDao.list());
+			model.addAttribute("listTemplate", templateDao.list());
 			return SOURCE_FORM;
 		}
-
 		sourceDao.saveOrUpdate(source);
-		
-		return "redirect:/sources/";
+		return REDIRECT_SOURCES;
 	}
+
+	@RequestMapping(value = "/{id}/re", method = RequestMethod.GET)
+	public String reShowSource(ModelMap model,@PathVariable String id) {
+		int sourceId = Integer.parseInt(id);
+		Source source = sourceDao.get(sourceId);
+		model.addAttribute("source", source);
+		return SOURCE_RE;
+	}
+	
+	@RequestMapping(value = "/{id}/reprocess", method = RequestMethod.GET)
+	public String reproccessSource(ModelMap model,@PathVariable String id) throws ComunicacaoComEditorialException {
+		int sourceId = Integer.parseInt(id);
+		Source source = sourceDao.get(sourceId);
+		ResultadoBuscaMateria resultadoBuscaConteudo = editorial.getListaUltimasNoticias(source.getProduct().getName());
+		model.addAttribute("source", source);
+		return SOURCE_RE;
+	}
+	
+	
+	
+	private void validateTemplateId(Source source, Errors errors) {
+	  if (source.getTemplate().getId() == null) errors.rejectValue("template", "validate.template.fail.mandatory_field");
+  }
+
+	private void validateProductId(Source source, Errors errors) {
+	  if (source.getProduct().getId() == null) errors.rejectValue("product", "validate.product.fail.mandatory_field");
+  }
 
 }
